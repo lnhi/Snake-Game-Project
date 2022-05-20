e549d102fd50fafd725436208d6bbdd3fda38010
Nguyen Nhi <100124914+lnhi@users.noreply.github.com>
2022-05-20 16:29:41 +0700

    20/5

--- SDL_utils.h
+++ SDL_utils.h
@@ -5,8 +5,12 @@
 #include <SDL_image.h>
 
 using namespace std;
+void logSDLError(std::ostream& os, const std:: string &msg, bool fatal);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer, int SCREEN_WIDTH, int SCREEN_HEIGHT, const string &WINDOW_TITLE);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *renderer);
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h);
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
 SDL_Surface* deleteBackGroundImage(SDL_Surface* image, Uint8 red, Uint8 green, Uint8 blue);
+void waitUntilKeyPressed();
 #endif // SDL_UTILS
